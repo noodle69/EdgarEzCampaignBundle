8520264dee308d843a6d2d91ae27e7eafb814a2c
Emmanuel DROUET <emmanuel.drouet@gmail.com>
2018-05-13 20:58:16 +0200

    remove deprecated classes

--- src/lib/Data/FolderCreateData.php
+++ src/lib/Data/FolderCreateData.php
@@ -3,12 +3,8 @@
 namespace Edgar\EzCampaign\Data;
 
 use EzSystems\RepositoryForms\Data\NewnessCheckable;
-use Edgar\EzCampaign\Values\API\Folder;
 use Edgar\EzCampaign\Values\FolderCreateStruct;
 
-/**
- * @property-read Folder $campaignFolder
- */
 class FolderCreateData extends FolderCreateStruct implements NewnessCheckable
 {
     use FolderDataTrait;
